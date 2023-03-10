5d70102a4778f9b042f21240116d2792983c31d8
manhpham-tda <manh.pham@tda.company>
2023-03-10 17:55:03 +0700

    header

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+
+const cx = classNames.bind(styles);
+
+function AccountItem(){
+    return (
+        <div className={cx('wrapper')}>
+            <img  className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/219e56290d96b5c4dae4c7750fb94a5b~c5_300x300.webp?x-expires=1678611600&x-signature=kxzjRIEwhMwkIexVsvUP8zK1tn4%3D" alt="Avatar"/>
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem
